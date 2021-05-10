d998c756eab50e80334cf4424e0946b70bfb7ff8
Timothy <timothyroy2011@gmail.com>
2021-05-10 15:26:21 +0600

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>March 28th 2020</div>
+      <div>
+        <h2>Title</h2>
+        <div>294.67</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
